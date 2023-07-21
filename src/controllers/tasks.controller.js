b68f25fbf5bab1462132911c29d317880456c99b
nkwaaaa <nkwaaaa@gmail.com>
2023-07-21 17:37:31 -0300

    add create task controller

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,8 +1,26 @@
+import { pool } from "../db.js"
+
 export const getAllTasks = (req, res) => res.send("obteniendo tareas")
 
 export const getTask = (req, res) => res.send("obteniendo tarea única")
 
-export const createTask = (req, res) => res.send("creando tarea")
+export const createTask = async (req, res, next) => {
+  const { title, description } = req.body
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    )
+
+    res.json(result.rows[0])
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.send("task already exist")
+    }
+    next(error)
+  }
+}
 
 export const updateTask = (req, res) => res.send("actualizando tarea única")
 
