0ef67e62853f93bdc0284de58991b1ee1415ace6
nkwaaaa <nkwaaaa@gmail.com>
2023-07-25 09:31:28 -0300

    add user register

--- database/init.sql
+++ database/init.sql
@@ -1,3 +1,12 @@
 CREATE TABLE task(id SERIAL PRIMARY KEY,
                                     title VARCHAR(255) UNIQUE NOT NULL,
                                                               description TEXT);
+
+
+CREATE TABLE users(id SERIAL PRIMARY KEY,
+                   name VARCHAR(255) NOT NULL,
+                   password VARCHAR(255) NOT NULL,
+                   email VARCHAR(255) UNIQUE NOT NULL,
+                   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                   );
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,28 @@
+import { pool } from "../db.js"
+import bcrypt from "bcrypt"
+
 export const signin = (req, res) => res.send("ingresando")
-export const signup = (req, res) => res.send("registrando")
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const result = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    )
+    console.log(result)
+
+    return res.json(result.rows[0])
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(400).json({ message: "Email is already registered" })
+    }
+  }
+}
+
 export const signout = (req, res) => res.send("cerrando sesion")
+
 export const profile = (req, res) => res.send("perfil del usuario")
