434e138e43818b49880fa28968694f1e54cbd98d
nkwaaaa <nkwaaaa@gmail.com>
2023-07-27 10:58:12 -0300

    todo react/display-name

---
+++ frontend/src/components/ui/Input copy.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/display-name */
+/* import { forwardRef } from "react"
+
+export const Input = forwardRef((props, ref) => {
+  return (
+    <input
+      type="text"
+      className="bg-zinc-800 px-3 py-2 block my-2 w-full"
+      ref={ref}
+      {...props}
+    />
+  )
+})
+ */
+//Input.displayName = "Input"
+
+//export default Input
+
+//otra
+
+/* export default function Input() {
+  return forwardRef(function test(props, ref) {
+    return (
+      <input
+        type="text"
+        className="bg-zinc-800 px-3 py-2 block my-2 w-full"
+        ref={ref}
+        {...props}
+      />
+    )
+  })
+} */
