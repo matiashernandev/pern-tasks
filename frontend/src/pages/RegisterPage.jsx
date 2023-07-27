086b5c061146d5658481453f305a13b771087272
nkwaaaa <nkwaaaa@gmail.com>
2023-07-27 10:55:56 -0300

    add register form

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,46 @@
+import { Button, Card, Input } from "../components/ui"
+import { useForm } from "react-hook-form"
+
 function RegisterPage() {
-  return <div>RegisterPage</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
+  return (
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h3>Register</h3>
+        <form onSubmit={onSubmit}>
+          <Input
+            {...register("name", { required: true })}
+            type="text"
+            placeholder="Enter your fullname"
+          />
+          {errors.name && <p className="text-red-500">Name is required</p>}
+          <Input
+            {...register("email", { required: true })}
+            type="email"
+            placeholder="Enter your email"
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+          <Input
+            {...register("password", { required: true })}
+            type="password"
+            placeholder="Enter your password"
+          />
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          <Button>Register</Button>
+        </form>
+      </Card>
+    </div>
+  )
 }
 export default RegisterPage
