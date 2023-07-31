88634a52ec0a31793f0279782ec20018a024eed3
nkwaaaa <nkwaaaa@gmail.com>
2023-07-31 10:02:35 -0300

    update deploy config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
 export const PGPORT = process.env.PGPORT || 5432
-export const PGHOST = process.env.HOST || "localhost"
+export const PGHOST = process.env.PGHOST || "localhost"
 export const PGUSER = process.env.PGUSER || "postgres"
 export const PGPASSWORD = process.env.PGPASSWORD || "admin"
 export const PGDATABASE = process.env.PGDATABASE || "tasksdb"
